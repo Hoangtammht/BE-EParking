c9c1711593e9e0b9c3623865cb901bfe9309fae5
Hoangtammht <hoangtammht@gmail.com>
2023-05-23 21:59:27 +0700

    Add Date for Parking and Pagination list parking

--- BE-EParking/src/main/java/com/eparking/eparking/dao/ParkingMapper.java
+++ BE-EParking/src/main/java/com/eparking/eparking/dao/ParkingMapper.java
@@ -1,11 +1,25 @@
 package com.eparking.eparking.dao;
 
 import com.eparking.eparking.domain.Parking;
+import com.eparking.eparking.domain.ParkingDate;
+import com.eparking.eparking.domain.ParkingSpecialDate;
+import com.eparking.eparking.domain.SpecialDate;
+import com.eparking.eparking.domain.response.ResponseParking;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @Mapper
 public interface ParkingMapper {
     void createParking(Parking parking);
+    ResponseParking findParkingByParkingID(int parkingID);
+    void addDatesForParking(List<ParkingDate> parkingDates);
+    void addSpecialDatesForParking(List<ParkingSpecialDate> parkingSpecialDates);
+    List<ParkingDate> showDatesOfParking(int parkingID);
+    void createSpecialDate(SpecialDate specialDate);
+    List<ResponseParking> getListParking(int size,int offset);
+    long getNumberOfParkings();
 
-    Parking findParkingByParkingID(int parkingID);
 }
